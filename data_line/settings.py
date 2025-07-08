4a37562e13aefc09ee5356572a881e94e1ef2a7b
khoshraftar <khoshraftar@divar.ir>
2025-07-08 19:09:02 +0330

    Update S3 storage configuration and settings

--- data_line/settings.py
+++ data_line/settings.py
@@ -175,7 +175,7 @@ AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
 
 # S3 Media settings only (static files remain local)
 AWS_S3_MEDIA_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'data_line.storage.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_S3_MEDIA_LOCATION}/'
 
 # S3 Settings
--- data_line/storage.py
+++ data_line/storage.py
@@ -1,9 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
+import os
 
 class MediaStorage(S3Boto3Storage):
-    location = getattr(settings, 'AWS_S3_MEDIA_LOCATION', 'media')
+    location = os.getenv('AWS_S3_MEDIA_LOCATION', 'media')
     default_acl = 'public-read'
-    file_overwrite = False
-    querystring_auth = False
-    custom_domain = getattr(settings, 'AWS_S3_CUSTOM_DOMAIN', None) 
+    file_overwrite = False 
