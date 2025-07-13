b14a1230100f8a5750e5329dd6b4c3d7b6d21c81
khoshraftar <khoshraftar@divar.ir>
2025-07-13 15:29:56 +0330

    Update settings.py

--- data_line/settings.py
+++ data_line/settings.py
@@ -109,12 +109,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  'db.sqlite3',
-    }
-}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
