b06b9226dd37ac66f2e45857bff1a16765e487b3
khoshraftar <khoshraftar@divar.ir>
2025-07-18 19:01:50 +0330

    Fix timezone import in utils.py

--- khodroyar/utils.py
+++ khodroyar/utils.py
@@ -1,7 +1,7 @@
 import jdatetime
 from datetime import datetime
 from .models import Payment
-from django.utils import timezone
+import pytz
 
 def to_shamsi_date(date_obj):
     """Convert datetime to Shamsi date format"""
@@ -55,7 +55,7 @@ def check_subscription_status(user_auth):
 """
     
     # Check if subscription has ended
-    if latest_payment.subscription_end and latest_payment.subscription_end < datetime.now(timezone.utc):
+    if latest_payment.subscription_end and latest_payment.subscription_end < datetime.now(tz=pytz.UTC):
         # Subscription has ended
         ended_message = f"""⏰ اشتراک شما به پایان رسیده است!
 📅 اشتراک شما در تاریخ {to_shamsi_date(latest_payment.subscription_end)} منقضی شده است.
